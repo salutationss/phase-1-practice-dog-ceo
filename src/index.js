b782d243f451cb5419831e2166452ceab394eb9a
Alex Blackburn <alex.blackburn1995@gmail.com>
2022-05-27 17:01:16 -0400

    commit tree c956537b37e48db3538afa2bdb223aef6c927b59

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+let img = name.map((imgName) =>{
+    return imgName
+} )
+
+fetch(imgUrl + img)
+.then(response => response.json())
+.then(img =>console.log(imgUrl + img))
+
 console.log('%c HI', 'color: firebrick')
